b12203826ef312e098d6d391a6979dba2efe7f65
lsc <68317275+kiki97-dev@users.noreply.github.com>
2024-04-04 16:38:03 +0900

    Feat: 마이페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ import Home from './pages/Home';
 import SingUp from './pages/SingUp';
 import Login from './pages/Login';
 import Notfound from './pages/Notfound';
+import Mypage from './pages/Mypage';
 
 import './swal-popup.scss';
 import './reset.css';
+import Reservation from './components/mypage/Reservation';
+import Account from './components/mypage/Account';
+import Point from './components/mypage/Point';
+import Review from './components/mypage/Review';
 
 function App() {
   return (
@@ -15,6 +20,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SingUp />} />
         <Route path="/login" element={<Login />} />
+
+        <Route path="/mypage" element={<Mypage />}>
+          <Route path="account" element={<Account />} />
+          <Route path="review" element={<Review />} />
+          <Route path="point" element={<Point />} />
+          <Route path="reservation" element={<Reservation />} />
+        </Route>
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
---
+++ src/components/mypage/Review.jsx
@@ -0,0 +1,11 @@
+function Review() {
+  return (
+    <>
+      <div className="mypage-review">
+        <h4>리뷰관리</h4>
+      </div>
+    </>
+  );
+}
+
+export default Review;
