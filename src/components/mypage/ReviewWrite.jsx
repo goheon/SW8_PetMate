a0cafa20c8bb020b45182ac678f7f0ec19e54c6b
lsc <68317275+kiki97-dev@users.noreply.github.com>
2024-04-10 15:38:56 +0900

    Feat : 리뷰 작성 페이지 추가

--- src/components/mypage/ReviewWrite.jsx
+++ src/components/mypage/ReviewWrite.jsx
@@ -1,10 +1,19 @@
 import { useRef, useState } from 'react';
 import Stars from '../Stars';
+import Select from 'react-select';
 
+//필터 옵션
+const options = [
+  { value: '1', label: <Stars rating={1}/> },
+  { value: '2', label: <Stars rating={2}/> },
+  { value: '3', label: <Stars rating={3}/> },
+  { value: '4', label: <Stars rating={4}/> },
+  { value: '5', label: <Stars rating={5}/> },
+ 
+];
 
 function ReviewWrite() {
-  const [experienceList, setExperienceList] = useState([]);
-  const [experienceValue, setExperienceValue] = useState();
+  const [selectedOption, setSelectedOption] = useState(options[0]);
   const [preview, setPreview] = useState();
   const fileInput = useRef();
 
@@ -18,35 +27,23 @@ function ReviewWrite() {
 
   const handleSummit = (e) => {
     e.preventDefault();
-    let types = [];
-    if (e.target.dog.checked) types.push('강아지');
-    if (e.target.cat.checked) types.push('고양이');
-    if (e.target.s.checked) types.push('소형');
-    if (e.target.m.checked) types.push('중형');
-    if (e.target.l.checked) types.push('대형');
 
     let formData = new FormData();
     formData.append('image', fileInput.current.files[0]);
 
     const data = {
-      type: types,
-      hourlyRate: {
-        priceS: Number(e.target.priceS.value),
-        priceM: Number(e.target.priceM.value),
-        priceL: Number(e.target.priceL.value),
-      },
-      experience: experienceList,
-      introduction: e.target.introduction.value,
+      introduction: e.target.reviewText.value,
       title: e.target.title.value,
       image: formData,
+      rating : Number(selectedOption.value),
     };
 
     console.log(data);
   };
   return (
     <>
-      <div className="mypage-join-expert">
-        <h4>리뷰작성</h4>
+      <div className="mypage-review-write">
+        <h4>리뷰작성 </h4>
         <form action="" onSubmit={handleSummit}>
           <div>
             <h6>이미지 업로드</h6>
@@ -60,7 +57,7 @@ function ReviewWrite() {
           <div>
             <h6>제목 작성</h6>
             <input
-              className="mypage-join-expert_title"
+              className="mypage-review-write_title"
               type="text"
               name="title"
               id="title"
@@ -68,14 +65,19 @@ function ReviewWrite() {
             />
           </div>
           <div>
-            <h6>리뷰 작성</h6>
-            <Stars rating={1} />
-            <textarea
-              id="introduction"
-              cols="30"
-              rows="10"
-              placeholder="리뷰 본문을 작성해주세요"
-            ></textarea>
+            <div className="review-write_wrap">
+              <h6>리뷰 작성</h6>
+              <div className="mypage-review-write_stars">
+                <Select
+                  className="mypage-review-write_stars-rate"
+                  placeholder={'전체상품'}
+                  options={options}
+                  onChange={setSelectedOption}
+                  defaultValue={selectedOption}
+                />
+              </div>
+            </div>
+            <textarea id="reviewText" cols="30" rows="10" placeholder="리뷰 본문을 작성해주세요"></textarea>
           </div>
           <button className="registration">리뷰 등록</button>
         </form>
