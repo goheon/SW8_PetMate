9456f2bdcf0760f74555c2565c924a67213222e2
이고헌 <gohoney22@gmail.com>
2024-04-13 16:22:52 +0900

    Feat: 개인정보 수정 API 연결, 컴포넌트명 수정
    회원 이름, 이메일, 비밀번호 API 연결 완료
    회원 탈퇴 API 연결 완료

--- src/components/mypage/Account.jsx
+++ src/components/mypage/Account.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import Swal from 'sweetalert2';
 
 import { setUserInfo } from '../../store';
-import { API_URL, getCookie } from '../../util/constants';
+import { API_URL } from '../../util/constants';
 
 function Account() {
+  const nav = useNavigate();
   const loginUserInfo = useSelector((state) => state.loginUserInfo);
   const [name, setName] = useState(loginUserInfo ? loginUserInfo.username : '');
   const [email, setEmail] = useState(loginUserInfo ? loginUserInfo.email : '');
@@ -23,14 +25,18 @@ function Account() {
   const [isEmailModify, setIsEmailModify] = useState(false);
   const [isPassWordModify, setIsPassWordModify] = useState(false);
 
-  const JWT = getCookie('jwt');
+  //로그아웃
+  const logout = () => {
+    document.cookie = 'jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    dispatch(setUserInfo(null));
+  };
 
   //수정요청
   async function updateUser(userInfo) {
     try {
       const response = await fetch(`${API_URL}/mypage`, {
         method: 'PUT',
-        headers: { 'Content-Type': 'application/json', Authorization: `Bearer ` + JWT },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(userInfo),
         credentials: 'include',
       });
@@ -56,7 +62,7 @@ function Account() {
     };
     dispatch(setUserInfo(userInfo));
     setIsNameModify(false);
-    updateUser(userInfo);
+    updateUser({ username: name });
   };
 
   //이메일 수정 버튼 핸들링
@@ -67,21 +73,58 @@ function Account() {
     };
     dispatch(setUserInfo(userInfo));
     setIsEmailModify(false);
-    updateUser(userInfo);
+    updateUser({ email: email });
   };
 
   //비밀번호 수정 버튼 핸들링
   const updatePassword = () => {
     const userInfo = {
       ...loginUserInfo,
-      password: password,
+      password: CryptoJS.SHA256(password).toString(),
     };
     dispatch(setUserInfo(userInfo));
     setIsPassWordModify(false);
-    updateUser(userInfo);
+    updateUser({ password: CryptoJS.SHA256(password).toString() });
   };
 
-  const handleWithdrawal = () => {};
+  //탈퇴버튼 핸들링
+  const handleWithdrawal = () => {
+    Swal.fire({
+      title: '정말로 탈퇴하시겠습니까?',
+      text: '',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: '확인',
+      cancelButtonText: '취소',
+      customClass: { container: 'custom-popup' },
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          //탈퇴요청
+          const response = await fetch(`${API_URL}/mypage/resign`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+          });
+
+          if (!response.ok) throw new Error('Network response was not ok');
+
+          Swal.fire({
+            title: '탈퇴완료',
+            text: '서비스를 이용해주셔서 감사합니다.',
+            icon: 'success',
+            customClass: { container: 'custom-popup' },
+          });
+
+          //탈퇴 후 로그아웃, 홈 이동
+          logout();
+          nav('/', { replace: true });
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      }
+    });
+  };
 
   return (
     <>
@@ -221,7 +264,9 @@ function Account() {
               <td>
                 <p>탈퇴 후 복구할 수 없습니다. 신중하게 결정해주세요.</p>
                 <br />
-                <button className="withdrawal">탈퇴하기</button>
+                <button className="withdrawal" onClick={handleWithdrawal}>
+                  탈퇴하기
+                </button>
               </td>
             </tr>
           </tbody>
