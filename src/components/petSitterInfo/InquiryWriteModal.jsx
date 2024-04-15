b4305e43e9e1f535318af5aa24353bb3e98a7812
lsc <68317275+kiki97-dev@users.noreply.github.com>
2024-04-15 19:26:45 +0900

    Feat:펫시터 상세페이지 API 연결

--- src/components/petSitterInfo/InquiryWriteModal.jsx
+++ src/components/petSitterInfo/InquiryWriteModal.jsx
@@ -16,15 +16,15 @@ const InquiryWriteModal = ({ isOpen, onClose, name }) => {
         <div className="modal-overlay" onClick={onClose}>
           <div className="modal" onClick={stopPropagation}>
             <button className="modal-close-btn" onClick={onClose}>
-              X
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>
             </button>
             <div className="modal_inner">
               <form action="submit" id="modal_form" className="modal_form" onSubmit={handleSubmit}>
                 <span className="inquiry">{name} 님에게 문의하기</span>
                 <span>제목</span>
-                <input type="text" className="title" />
+                <input type="text" className="title" placeholder='제목을 입력하세요.' />
                 <span>내용</span>
-                <textarea name="content" id="content" cols="30" rows="10" className="content"></textarea>
+                <textarea placeholder='문의내용을 입력하세요.' name="content" id="content" cols="30" rows="5" className="content"></textarea>
                 <div className="submit-button-wrapper">
                   <button type="submit" className="modal_submit">
                     문의하기
--- src/pages/PetSitterInfo.jsx
+++ src/pages/PetSitterInfo.jsx
@@ -2,10 +2,45 @@ import { useState, useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { getCookie, parseJwt } from '../util/constants';
+import { API_URL, getCookie, parseJwt } from '../util/constants';
 import { MyDatePicker, TimePicker } from '../components/datepicker/DatePicker';
 import InquiryWriteModal from '../components/petSitterInfo/InquiryWriteModal';
 import './PetSitterInfo.scss';
+import { useParams } from 'react-router-dom';
+
+//펫시터 정보 가지구오기
+async function getPetSitterInfo(sitterId) {
+  try {
+    const response = await fetch(`${API_URL}/orderSitter/${sitterId}`, {
+      method: 'GET',
+    });
+
+    if (!response.ok) throw new Error('Network response was not ok');
+
+    const data = await response.json();
+    if (data) return data;
+  } catch (error) {
+    console.log('Error:', error);
+  }
+}
+
+//예약요청
+async function postRequest(sitterId, data) {
+  try {
+    const response = await fetch(`${API_URL}/orderSitter/${sitterId}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+      credentials: 'include',
+    });
+
+    if (!response.ok) throw new Error('Network response was not ok');
+
+    console.log(response);
+  } catch (error) {
+    console.log('Error:', error);
+  }
+}
 
 // 리뷰(리뷰 수, 펫시터의 리뷰 목록), User(주소(활동범위)) 정보를 가져옴
 PetSitterInfo.defaultProps = {
@@ -30,7 +65,7 @@ PetSitterInfo.defaultProps = {
   hourlyRate: { small: 15000, medium: 20000, large: 25000, cat: 10000 },
 };
 
-function PetSitterInfo({ img, sitterId, name, type, location, title, introduction, experience, check, hourlyRate }) {
+function PetSitterInfo({ img, name, type, location, title, introduction, experience, check, hourlyRate }) {
   const petTypeRef = useRef();
   const petCountRef = useRef();
   const requestRef = useRef();
@@ -41,6 +76,16 @@ function PetSitterInfo({ img, sitterId, name, type, location, title, introductio
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedPetList, setSelectedpetList] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
+  const { sitterId } = useParams(); //
+  //펫시터 데이터
+  const [petSitterData, setPetSitterData] = useState();
+
+  useEffect(() => {
+    getPetSitterInfo(sitterId).then((data) => {
+      setPetSitterData(data);
+    });
+  }, []);
+  console.log(petSitterData);
 
   //시간 변경을 감시
   useEffect(() => {
@@ -224,7 +269,7 @@ function PetSitterInfo({ img, sitterId, name, type, location, title, introductio
       new Date(endTime).getHours(),
     );
 
-    console.log(
+    /* console.log(
       'pets: ',
       pets,
       'userId: ',
@@ -239,8 +284,22 @@ function PetSitterInfo({ img, sitterId, name, type, location, title, introductio
       formedStartDate,
       'formedEndDate: ',
       formedEndDate,
-    );
+    ); */
+
+    const data = {
+      pets,
+      totalPrice,
+      detailInfo,
+      startDate: formedStartDate,
+      endDate: formedEndDate,
+    };
+
+    console.log(data);
+
+    postRequest(sitterId, data);
   };
+
+  if (!petSitterData) return <>로딩즁이거나 없는 펫시터 정보입니다</>;
 
   return (
     <>
@@ -248,6 +307,72 @@ function PetSitterInfo({ img, sitterId, name, type, location, title, introductio
         <Header />
         <section className="container">
           <div className="container_left">
+            <div className="pet-sitter-Introduction">
+              <div className="pet-sitter-Introduction_title">
+                <p>서울 강남구 파트너 &middot; 이하은 님</p>
+                <h5>{petSitterData.title}</h5>
+                <div className="pet-sitter-Introduction_tag">
+                  {petSitterData.type.map((el, index) => {
+                    return <span key={index}>#{el}</span>;
+                  })}
+                </div>
+              </div>
+              <div className="pet-sitter-Introduction_detail">
+                <h5>이하은 펫시터님을 소개합니다</h5>
+                <p>{petSitterData.introduction}</p>
+              </div>
+              <div className="pet-sitter-Introduction_service">
+                <h5>이용 가능 서비스</h5>
+                <ul>
+                  <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path d="M226.5 92.9c14.3 42.9-.3 86.2-32.6 96.8s-70.1-15.6-84.4-58.5s.3-86.2 32.6-96.8s70.1 15.6 84.4 58.5zM100.4 198.6c18.9 32.4 14.3 70.1-10.2 84.1s-59.7-.9-78.5-33.3S-2.7 179.3 21.8 165.3s59.7 .9 78.5 33.3zM69.2 401.2C121.6 259.9 214.7 224 256 224s134.4 35.9 186.8 177.2c3.6 9.7 5.2 20.1 5.2 30.5v1.6c0 25.8-20.9 46.7-46.7 46.7c-11.5 0-22.9-1.4-34-4.2l-88-22c-15.3-3.8-31.3-3.8-46.6 0l-88 22c-11.1 2.8-22.5 4.2-34 4.2C84.9 480 64 459.1 64 433.3v-1.6c0-10.4 1.6-20.8 5.2-30.5zM421.8 282.7c-24.5-14-29.1-51.7-10.2-84.1s54-47.3 78.5-33.3s29.1 51.7 10.2 84.1s-54 47.3-78.5 33.3zM310.1 189.7c-32.3-10.6-46.9-53.9-32.6-96.8s52.1-69.1 84.4-58.5s46.9 53.9 32.6 96.8s-52.1 69.1-84.4 58.5z" />
+                    </svg>
+                    <div>
+                      <strong>퍼피 산책케어</strong>
+                      <p>1년 미만 퍼피 가능</p>
+                    </div>
+                  </li>
+                  <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                      <path d="M416 0C352.3 0 256 32 256 32V160c48 0 76 16 104 32s56 32 104 32c56.4 0 176-16 176-96S512 0 416 0zM128 96c0 35.3 28.7 64 64 64h32V32H192c-35.3 0-64 28.7-64 64zM288 512c96 0 224-48 224-128s-119.6-96-176-96c-48 0-76 16-104 32s-56 32-104 32V480s96.3 32 160 32zM0 416c0 35.3 28.7 64 64 64H96V352H64c-35.3 0-64 28.7-64 64z" />
+                    </svg>
+                    <div>
+                      <strong>매일 산책</strong>
+                      <p>산책 및 실외 배변 가능</p>
+                    </div>
+                  </li>
+                  <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                      <path d="M290.8 48.6l78.4 29.7L288 109.5 206.8 78.3l78.4-29.7c1.8-.7 3.8-.7 5.7 0zM136 92.5V204.7c-1.3 .4-2.6 .8-3.9 1.3l-96 36.4C14.4 250.6 0 271.5 0 294.7V413.9c0 22.2 13.1 42.3 33.5 51.3l96 42.2c14.4 6.3 30.7 6.3 45.1 0L288 457.5l113.5 49.9c14.4 6.3 30.7 6.3 45.1 0l96-42.2c20.3-8.9 33.5-29.1 33.5-51.3V294.7c0-23.3-14.4-44.1-36.1-52.4l-96-36.4c-1.3-.5-2.6-.9-3.9-1.3V92.5c0-23.3-14.4-44.1-36.1-52.4l-96-36.4c-12.8-4.8-26.9-4.8-39.7 0l-96 36.4C150.4 48.4 136 69.3 136 92.5zM392 210.6l-82.4 31.2V152.6L392 121v89.6zM154.8 250.9l78.4 29.7L152 311.7 70.8 280.6l78.4-29.7c1.8-.7 3.8-.7 5.7 0zm18.8 204.4V354.8L256 323.2v95.9l-82.4 36.2zM421.2 250.9c1.8-.7 3.8-.7 5.7 0l78.4 29.7L424 311.7l-81.2-31.1 78.4-29.7zM523.2 421.2l-77.6 34.1V354.8L528 323.2v90.7c0 3.2-1.9 6-4.8 7.3z" />
+                    </svg>
+                    <div>
+                      <strong>실내 놀이</strong>
+                      <p>터그놀이, 노즈워크 등</p>
+                    </div>
+                  </li>
+                  <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                      <path d="M192 48c0-26.5 21.5-48 48-48H400c26.5 0 48 21.5 48 48V512H368V432c0-26.5-21.5-48-48-48s-48 21.5-48 48v80H192V48zM48 96H160V512H48c-26.5 0-48-21.5-48-48V320H80c8.8 0 16-7.2 16-16s-7.2-16-16-16H0V224H80c8.8 0 16-7.2 16-16s-7.2-16-16-16H0V144c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48v48H560c-8.8 0-16 7.2-16 16s7.2 16 16 16h80v64H560c-8.8 0-16 7.2-16 16s7.2 16 16 16h80V464c0 26.5-21.5 48-48 48H480V96H592zM312 64c-8.8 0-16 7.2-16 16v24H272c-8.8 0-16 7.2-16 16v16c0 8.8 7.2 16 16 16h24v24c0 8.8 7.2 16 16 16h16c8.8 0 16-7.2 16-16V152h24c8.8 0 16-7.2 16-16V120c0-8.8-7.2-16-16-16H344V80c0-8.8-7.2-16-16-16H312z" />
+                    </svg>
+                    <div>
+                      <strong>응급 처치</strong>
+                      <p>응급 상황 시 병원 이동 가능</p>
+                    </div>
+                  </li>
+                  <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                      <path d="M112 96c-26.5 0-48 21.5-48 48V256h96V144c0-26.5-21.5-48-48-48zM0 144C0 82.1 50.1 32 112 32s112 50.1 112 112V368c0 61.9-50.1 112-112 112S0 429.9 0 368V144zM554.9 399.4c-7.1 12.3-23.7 13.1-33.8 3.1L333.5 214.9c-10-10-9.3-26.7 3.1-33.8C360 167.7 387.1 160 416 160c88.4 0 160 71.6 160 160c0 28.9-7.7 56-21.1 79.4zm-59.5 59.5C472 472.3 444.9 480 416 480c-88.4 0-160-71.6-160-160c0-28.9 7.7-56 21.1-79.4c7.1-12.3 23.7-13.1 33.8-3.1L498.5 425.1c10 10 9.3 26.7-3.1 33.8z" />
+                    </svg>
+                    <div>
+                      <strong>약물 복용</strong>
+                      <p>경구(입) 약물 복용 가능</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
             <section className="review-section">
               <div className="review-card_inner">
                 <div className="review">
@@ -266,16 +391,9 @@ function PetSitterInfo({ img, sitterId, name, type, location, title, introductio
                     exercitation irure fugiat et culpa velit proident velit.
                   </p>
                   <div className="review_images">
-                    <img
-                      alt="user-img"
-                      className="review_user-profile_img"
-                      src="https://tmpfiles.nohat.cc/abstract-user-flat-3.svg"
-                    />
-                    <img
-                      alt="user-img"
-                      className="review_user-profile_img"
-                      src="https://tmpfiles.nohat.cc/abstract-user-flat-3.svg"
-                    />
+                    <img alt="user-img" className="review_user-profile_img" src="/main02_review_01.jpg" />
+                    <img alt="user-img" className="review_user-profile_img" src="/main02_review_02.jpg" />
+                    <img alt="user-img" className="review_user-profile_img" src="/main_banner_01.png" />
                   </div>
                   <div className="review_pet-sitter">
                     <div className="review_pet-sitter_profile">
@@ -294,6 +412,7 @@ function PetSitterInfo({ img, sitterId, name, type, location, title, introductio
                     </div>
                   </div>
                 </div>
+
                 <div className="review">
                   <div className="review_user-profile">
                     <img
@@ -310,16 +429,47 @@ function PetSitterInfo({ img, sitterId, name, type, location, title, introductio
                     exercitation irure fugiat et culpa velit proident velit.
                   </p>
                   <div className="review_images">
+                    <img alt="user-img" className="review_user-profile_img" src="/main02_review_01.jpg" />
+                    <img alt="user-img" className="review_user-profile_img" src="/main02_review_02.jpg" />
+                    <img alt="user-img" className="review_user-profile_img" src="/main_banner_01.png" />
+                  </div>
+                  <div className="review_pet-sitter">
+                    <div className="review_pet-sitter_profile">
+                      <img
+                        className="review_pet-sitter_img"
+                        src="https://dispatch.cdnser.be/cms-content/uploads/2020/10/22/bd74cb66-a4ef-4c57-9358-1cb0494d9dc2.jpg"
+                        alt="pet-siiter-img"
+                      />
+                      <span>{name}</span>
+                    </div>
+                    <div className="review_pet-siiter_comment">
+                      Voluptate laboris incididunt elit quis sit fugiat. Quis id do consectetur non id do tempor esse
+                      mollit et ullamco reprehenderit qui. Veniam nisi reprehenderit laborum non. Ad exercitation amet
+                      dolor exercitation. Duis consectetur sint sunt in esse velit aute mollit nulla eu et. Magna ad
+                      nulla aliqua sint reprehenderit.
+                    </div>
+                  </div>
+                </div>
+
+                <div className="review">
+                  <div className="review_user-profile">
                     <img
                       alt="user-img"
                       className="review_user-profile_img"
                       src="https://tmpfiles.nohat.cc/abstract-user-flat-3.svg"
                     />
-                    <img
-                      alt="user-img"
-                      className="review_user-profile_img"
-                      src="https://tmpfiles.nohat.cc/abstract-user-flat-3.svg"
-                    />
+                    <span className="review_user-profile_name">이고헌</span>
+                  </div>
+                  <p className="review_user-comment">
+                    Pariatur mollit magna commodo qui culpa Lorem qui esse culpa minim nisi. Esse laboris reprehenderit
+                    magna consectetur ullamco nisi sit. Eu ullamco ad elit cupidatat sint enim pariatur nostrud sit
+                    nostrud sit deserunt laborum proident. Consequat anim mollit nulla nulla labore pariatur
+                    exercitation irure fugiat et culpa velit proident velit.
+                  </p>
+                  <div className="review_images">
+                    <img alt="user-img" className="review_user-profile_img" src="/main02_review_01.jpg" />
+                    <img alt="user-img" className="review_user-profile_img" src="/main02_review_02.jpg" />
+                    <img alt="user-img" className="review_user-profile_img" src="/main_banner_01.png" />
                   </div>
                   <div className="review_pet-sitter">
                     <div className="review_pet-sitter_profile">
@@ -364,7 +514,7 @@ function PetSitterInfo({ img, sitterId, name, type, location, title, introductio
               </div>
               <div className="petsitter_mid">
                 <ul>
-                  {experience.map((info, i) => {
+                  {petSitterData.experience.map((info, i) => {
                     return (
                       <li key={i}>
                         <p>{info}</p>
@@ -388,6 +538,7 @@ function PetSitterInfo({ img, sitterId, name, type, location, title, introductio
                 </p>
               </div>
             </div>
+
             <section className="reservation-section">
               <div className="reservation-card_inner">
                 <InquiryWriteModal isOpen={isModalOpen} onClose={closeModal} name={name} />
@@ -471,7 +622,9 @@ function PetSitterInfo({ img, sitterId, name, type, location, title, introductio
                     <button type="button" className="inquiryWrite" onClick={openModal}>
                       문의하기
                     </button>
-                    <button type="submit">예약 요청</button>
+                    <button className="request-btn" type="submit">
+                      예약 요청
+                    </button>
                   </div>
                 </form>
               </div>
