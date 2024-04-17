690725a8496b1272f4610749211b75547dde22e9
이고헌 <gohoney22@gmail.com>
2024-04-17 16:36:31 +0900

    Feat: 마이페이지 개인정보 주소수정 추가

--- src/components/mypage/Account.jsx
+++ src/components/mypage/Account.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import Swal from 'sweetalert2';
 
+import AddressAPI from '../AddressAPI';
 import { ButtonLoading } from '../Spinner';
 import { setUserInfo } from '../../store';
 import { fetchWithdrawal, fetchUpdateUser } from './util/APIrequest';
@@ -21,6 +22,10 @@ function Account() {
   const loginUserInfo = useSelector((state) => state.loginUserInfo);
   const [name, setName] = useState(loginUserInfo ? loginUserInfo.username : '');
   const [phone, setPhone] = useState(loginUserInfo ? loginUserInfo.phone : '');
+  const [address, setAddress] = useState(loginUserInfo ? loginUserInfo.address : '');
+  const [address2, setAddress2] = useState(loginUserInfo ? loginUserInfo.address : '');
+  const [detailAddress, setDetailAddress] = useState(loginUserInfo ? loginUserInfo.detailAddress : '');
+  const [isOpen, setIsOpen] = useState(false);
   const [password, setPassword] = useState('');
   const dispatch = useDispatch();
 
@@ -28,15 +33,24 @@ function Account() {
     if (loginUserInfo) {
       setName(loginUserInfo.username);
       setPhone(loginUserInfo.phone);
+      setAddress2(loginUserInfo.address);
+      setDetailAddress(loginUserInfo.detailAddress);
     }
   }, [loginUserInfo]);
 
+  useEffect(() => {
+    setAddress2(address.address);
+  }, [address]);
+
   const [isNameModify, setIsNameModify] = useState(false);
   const [isPhoneModify, setIsPhoneModify] = useState(false);
+  const [isAddressModify, setIsAddressModify] = useState(false);
   const [isPassWordModify, setIsPassWordModify] = useState(false);
+
   const [nameLoadingState, setNameLoadingState] = useState(false);
   const [phoneLoadingState, setPhoneLoadingState] = useState(false);
   const [pwLoadingState, setPwLoadingState] = useState(false);
+  const [addressLoadingState, setAddressLoadingState] = useState(false);
 
   //이름수정 버튼 핸들링
   const updateName = async () => {
@@ -61,10 +75,10 @@ function Account() {
     };
     setPhoneLoadingState(true);
     dispatch(setUserInfo(userInfo));
-    setIsPhoneModify(false);
     const response = await fetchUpdateUser({ phone: phone });
     if (!response.ok) throw new Error('Network response was not ok');
     setPhoneLoadingState(false);
+    setIsPhoneModify(false);
     alertEditComplete();
   };
 
@@ -76,10 +90,26 @@ function Account() {
     };
     setPwLoadingState(true);
     dispatch(setUserInfo(userInfo));
-    setIsPassWordModify(false);
     const response = await fetchUpdateUser({ password: CryptoJS.SHA256(password).toString() });
     if (!response.ok) throw new Error('Network response was not ok');
     setPwLoadingState(false);
+    setIsPassWordModify(false);
+    alertEditComplete();
+  };
+
+  //주소 수정 핸들링
+  const updateAddress = async () => {
+    const userInfo = {
+      ...loginUserInfo,
+      address: address2,
+      detailAddress: detailAddress,
+    };
+    setAddressLoadingState(true);
+    dispatch(setUserInfo(userInfo));
+    const response = await fetchUpdateUser({ address: address2, detailAddress: detailAddress });
+    if (!response.ok) throw new Error('Network response was not ok');
+    setAddressLoadingState(false);
+    setIsAddressModify(false);
     alertEditComplete();
   };
 
@@ -122,13 +152,13 @@ function Account() {
       customClass: { container: 'custom-popup' },
     });
   }
+
   //로그아웃
   const logout = () => {
     document.cookie = 'jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
     dispatch(setUserInfo(null));
   };
 
-  console.log(loginUserInfo);
   return (
     <>
       <div className="mypage-account">
@@ -253,6 +283,73 @@ function Account() {
                       <svg
                         onClick={() => {
                           setIsPassWordModify(true);
+                        }}
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                      >
+                        <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+                      </svg>
+                    </>
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <td>주소</td>
+                <td>
+                  {isAddressModify ? (
+                    <>
+                      <div className="address-wrap">
+                        <input
+                          value={address2}
+                          disabled
+                          id="edit-address"
+                          type="text"
+                          name="address"
+                          placeholder="주소"
+                        />
+                        <button
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setIsOpen(true);
+                          }}
+                          className="ja_find"
+                        >
+                          우편번호 찾기
+                        </button>
+                        {isOpen && <AddressAPI setIsOpen={setIsOpen} setInputValues={setAddress} />}
+                      </div>
+                      <div className="address-wrap-2">
+                        <br />
+                        <input
+                          type="text"
+                          name="detailAdress"
+                          value={detailAddress}
+                          onChange={(e) => {
+                            setDetailAddress(e.target.value);
+                          }}
+                        />
+
+                        <button
+                          onClick={() => {
+                            setAddress(loginUserInfo ? `${loginUserInfo.address} ${loginUserInfo.detailAddress}` : '');
+                            setIsAddressModify(false);
+                            console.log(address, detailAddress);
+                          }}
+                          className="cancel"
+                        >
+                          취소
+                        </button>
+                        <button onClick={updateAddress}>
+                          {addressLoadingState === true ? <ButtonLoading size={15} /> : '수정'}
+                        </button>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <p>{loginUserInfo ? `${loginUserInfo.address} ${loginUserInfo.detailAddress}` : ''}</p>
+                      <svg
+                        onClick={() => {
+                          setIsAddressModify(true);
                         }}
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 512 512"
