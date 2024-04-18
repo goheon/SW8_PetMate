30b3f682270df08b30c774722e6c13f54ae04147
lsc <68317275+kiki97-dev@users.noreply.github.com>
2024-04-18 16:15:19 +0900

    Fix : 상세페이지 후기리뷰추가

--- src/components/petSitterList/PetSitterCard.jsx
+++ src/components/petSitterList/PetSitterCard.jsx
@@ -5,62 +5,62 @@ import { cutAddressToDistrict } from '../../util/constants';
 import './PetSitterCard.scss';
 
 function PetSitterCard(props) {
-    const navigate = useNavigate();
-    const typeMapping = {
-        small: '소형견',
-        medium: '중형견',
-        large: '대형견',
-        cat: '고양이',
-    };
+  const navigate = useNavigate();
+  const typeMapping = {
+    small: '소형견',
+    medium: '중형견',
+    large: '대형견',
+    cat: '고양이',
+  };
 
-    const handleCardClick = (sitterId) => {
-        navigate(`/pet-sitter/${sitterId}`);
-    }
+  const handleCardClick = (sitterId) => {
+    navigate(`/pet-sitter/${sitterId}`);
+  };
 
-    return (
-        <div className='search-list_inner'>
-            <div className='search_wrap' onClick={() => (handleCardClick(props.sitter.sitterId))}>
-                <div className='img-box'>
-                    <img src={props.sitter.image} />
-                    {
-                        console.log(props.sitter.image)
-                    }
-                </div>
-                <div className='text-box'>
-                    <div className='text-box_top'>
-                        <p>{cutAddressToDistrict(props.sitter.address)}&nbsp;파트너&nbsp;&middot;&nbsp;{props.sitter.name}&nbsp;님</p>
-                        <h4>{props.sitter.title}</h4>
-                    </div>
-                    <div className='text-box_bottom'>
-                        <div className='tb_left'>
-                            <div className='tb_keyword'>
-                                {
-                                    props.sitter.type.map((type, index) => (<span key={index}>#{type}</span>))
-                                }
-                            </div>
-                            <div className='tb_review'>
-                                <Stars rating={5} />
-                                <p>후기 8개</p>
-                            </div>
-                        </div>
-                        <div className='tb_right'>
-
-                            {
-                                Object.entries(props.sitter.hourlyRate).map(([size, price], index) => (
-                                    <div className='tr_price' key={index}>
-                                        <div>
-                                            <p>{typeMapping[size]}</p>
-                                        </div>
-                                        <p>&#x20A9;{price.toLocaleString()}<span>/시간</span></p>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="search-list_inner">
+      <div className="search_wrap" onClick={() => handleCardClick(props.sitter.sitterId)}>
+        <div className="img-box">
+          <img src={props.sitter.image} />
+          {console.log(props.sitter.image)}
         </div>
-    )
+        <div className="text-box">
+          <div className="text-box_top">
+            <p>
+              {cutAddressToDistrict(props.sitter.address)}&nbsp;파트너&nbsp;&middot;&nbsp;{props.sitter.name}&nbsp;님
+            </p>
+            <h4>{props.sitter.title}</h4>
+          </div>
+          <div className="text-box_bottom">
+            <div className="tb_left">
+              <div className="tb_keyword">
+                {props.sitter.type.map((type, index) => (
+                  <span key={index}>#{type}</span>
+                ))}
+              </div>
+              <div className="tb_review">
+                <Stars rating={props.sitter.avgStar} />
+                <p>후기 {props.sitter.totalReviews}개</p>
+              </div>
+            </div>
+            <div className="tb_right">
+              {Object.entries(props.sitter.hourlyRate).map(([size, price], index) => (
+                <div className="tr_price" key={index}>
+                  <div>
+                    <p>{typeMapping[size]}</p>
+                  </div>
+                  <p>
+                    &#x20A9;{price.toLocaleString()}
+                    <span>/시간</span>
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PetSitterCard;
